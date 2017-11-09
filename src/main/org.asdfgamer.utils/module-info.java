7edcfa0368d29339e92617ce5074e58a976bb7fb
ASDFGamer <ASDFGamer2212@outlook.de>
2017-11-09 19:09:10 +0100

    Zu Jingsaw Modul umgewandelt.

---
+++ src/main/org.asdfgamer.utils/module-info.java
@@ -0,0 +1,8 @@
+module org.asdfgamer.utils {
+    requires commons.net;
+    requires java.logging;
+    requires javafx.base;
+    exports org.asdfgamer.utils.config;
+    exports org.asdfgamer.utils.file;
+    exports org.asdfgamer.utils.other;
+}
